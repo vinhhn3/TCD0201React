3c1d235c97d7aa2a7762091c2c3e21e9289bf8bd
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-21 15:34:54 +0700

    Components and Props

--- tcd0201/src/App.js
+++ tcd0201/src/App.js
@@ -1,36 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
-  state = {
-    count: 0,
-    name: "Vinh Hoang",
-    showName: true,
-    showLoading: false,
-  };
-
-  increase = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  decrease = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-
   render() {
     return (
       <div className="App">
-        <h1>Hello World !!!</h1>
-        <p>Count: {this.state.count}</p>
-        <button onClick={this.increase}>Increase</button>
-        <button onClick={this.decrease}>Decrease</button>
-        <p>Full Name: {this.state.showName && this.state.name}</p>
-        {this.state.showLoading ? (
-          <h4>Loading ...</h4>
-        ) : (
-          <h2>Display Result</h2>
-        )}
+        <Navbar title="Welcome everybody!!!" />
       </div>
     );
   }
