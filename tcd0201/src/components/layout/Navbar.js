d87ba85a6286c380f43830500cb1c689ce265849
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-26 08:07:27 +0700

    Convert Navbar from Class component to Function component

--- tcd0201/src/components/layout/Navbar.js
+++ tcd0201/src/components/layout/Navbar.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div className="navbar bg-primary">
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
-}
+// class Navbar extends Component {
+//   render() {
+//     return (
+//       <div className="navbar bg-primary">
+//         <h1>{this.props.title}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+const Navbar = ({ title }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>{title}</h1>
+    </div>
+  );
+};
 
 export default Navbar;
