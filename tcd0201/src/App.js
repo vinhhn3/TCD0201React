8965d658a24c23a2ccc9699756f35bd0f644aaf3
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-26 10:36:55 +0700

    Setup React Router

--- tcd0201/src/App.js
+++ tcd0201/src/App.js
@@ -1,24 +1,17 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import axios from "axios";
 import Search from "./components/users/Search";
+import About from "./pages/About";
 
 class App extends Component {
   state = {
     users: [],
     showLoading: false,
   };
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const response = await axios.get(
-  //     "https://api.github.com/search/users?q=vinh"
-  //   );
-  //   console.log(response.data.items);
-  //   this.setState({ users: response.data.items, showLoading: false });
-  // }
 
   searchUsers = async (text) => {
     this.setState({ showLoading: true });
@@ -34,20 +27,33 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar title="TCD0201React" />
-        <div className="container">
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            users={this.state.users}
-          />
-          <Users
-            users={this.state.users}
-            showLoading={this.state.showLoading}
-          />
+      <Router>
+        <div className="App">
+          <Navbar title="TCD0201React" />
+          <div className="container">
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      users={this.state.users}
+                    />
+                    <Users
+                      users={this.state.users}
+                      showLoading={this.state.showLoading}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
