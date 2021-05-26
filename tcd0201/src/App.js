a3bfc9b8e10140433da137eba50b9a7df4a95a40
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-26 10:03:55 +0700

    Passing up Props and Search Users

--- tcd0201/src/App.js
+++ tcd0201/src/App.js
@@ -8,21 +8,32 @@ import Search from "./components/users/Search";
 class App extends Component {
   state = {
     users: [],
-    showLoading: true,
+    showLoading: false,
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const response = await axios.get("https://api.github.com/users");
-    this.setState({ users: response.data, showLoading: false });
-  }
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const response = await axios.get(
+  //     "https://api.github.com/search/users?q=vinh"
+  //   );
+  //   console.log(response.data.items);
+  //   this.setState({ users: response.data.items, showLoading: false });
+  // }
+
+  searchUsers = async (text) => {
+    this.setState({ showLoading: true });
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${text}`
+    );
+    this.setState({ users: response.data.items, showLoading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar title="TCD0201React" />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users
             users={this.state.users}
             showLoading={this.state.showLoading}
