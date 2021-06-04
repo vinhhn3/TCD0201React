5750295ceaaba898f49a412f354e1f093ac9d8b3
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-06-04 15:42:47 +0700

    UserItem component to functional component

--- tcd0201/src/components/users/UserItem.js
+++ tcd0201/src/components/users/UserItem.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img src={avatar_url} className="round-img" style={{ width: "60px" }} />
-        <h3>{login}</h3>
-        <div>
-          <Link to={`user/${login}`} className="btn btn-primary btn-sm">More</Link>
-        </div>
+const UserItem = (props) => {
+  const { login, avatar_url } = props.user;
+  return (
+    <div className="card text-center">
+      <img src={avatar_url} className="round-img" style={{ width: "60px" }} />
+      <h3>{login}</h3>
+      <div>
+        <Link to={`user/${login}`} className="btn btn-primary btn-sm">
+          More
+        </Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
--- tcd0201/src/pages/About.js
+++ tcd0201/src/pages/About.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 
 const About = () => {
-    return (
-      <div>
-        <h1>This is about page</h1>
-      </div>
-    );
-}
+  return (
+    <div>
+      <h1>This is about page</h1>
+    </div>
+  );
+};
 
 export default About;
