e948fc92883b94bc8bd393bea66d9a28c8a4a92d
Hoang Nguyen <hoang.nguyen@team.enouvo.com>
2021-06-11 10:48:24 +0700

    add labs

---
+++ hoangnv/Lab2/src/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './NotFound.css';
+
+export default function Login() {
+  return <div className="FourOFour">
+    <h1>Something went wrong...</h1>
+    <img src="/blue-origami-bird.png" alt="404" />
+  </div>;
+}
